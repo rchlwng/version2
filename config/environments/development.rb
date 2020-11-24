1c1282df6d814d8964abdc5b5c2ea17cffa59fcb
rchlwng <rachel@lu-wang.com>
2020-11-24 12:46:21 -0800

    env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'rachel@lu-wang.com',
-    password:             '1KzT0zrYCbRt',
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
