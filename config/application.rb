265ab24f5e36ff57f0337b6d23cf657117bc4691
rchlwng <rachel@lu-wang.com>
2020-12-11 10:18:03 -0800

    mailgun in application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Rachie
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.delivery_method = :mailgun
+    config.action_mailer.mailgun_settings = {
+      api_key: ENV["MAILGUN_API_KEY"],
+      domain: ENV["MAILGUN_DOMAIN"],
+      # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+    }
   end
 end
